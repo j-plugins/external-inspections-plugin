e859a5fa3ee0b6a5ad84a1539fba5e2c1ba31062
Dmitriy Derepko <xepozz@list.ru>
2026-01-24 13:19:41 +0400

    fix: compatibility with 2023.3

--- src/main/kotlin/com/github/xepozz/external_inspections/ExternalInspectionsResourceProvider.kt
+++ src/main/kotlin/com/github/xepozz/external_inspections/ExternalInspectionsResourceProvider.kt
@@ -6,14 +6,10 @@ import com.intellij.javaee.StandardResourceProvider
 class ExternalInspectionsResourceProvider : StandardResourceProvider {
     override fun registerResources(registrar: ResourceRegistrar) {
         registrar.addStdResource(
-            "https://raw.githubusercontent.com/j-plugins/external-inspections-plugin/main/src/main/resources/external-inspections.xsd",
-            "external-inspections.xsd",
-            javaClass.classLoader,
-        )
-        registrar.addStdResource(
             "http://j-plugins.github.io/external-inspections.xsd",
-            "external-inspections.xsd",
-            javaClass.classLoader,
+            null,
+            "/external-inspections.xsd",
+            javaClass,
         )
     }
 }
